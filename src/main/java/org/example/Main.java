5ce61ea88167d92862840d3030f81743dc65dc8c
2021 <yarmail@yandex.ru>
2024-12-15 22:37:07 +0300

    update

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,47 @@
+package org.example;
+
+import java.io.IOException;
+import java.nio.file.*;
+import java.util.Deque;
+import java.util.ArrayDeque;
+
+/**
+ * утилита для поиска длинных путей
+ */
+public class Main {
+  private static final int MAX_PATH_LENGTH = 250;
+  private static final String stringPathToFolder = "E:\\";
+
+  public static void main(String[] args) {
+    Path pathToFolder = Paths.get(stringPathToFolder);
+    Deque<Path> stack = new ArrayDeque<>();
+    stack.push(pathToFolder);
+
+    while (!stack.isEmpty()) {
+      Path currentDir = stack.pop();
+
+      try(DirectoryStream<Path> stream = Files.newDirectoryStream(currentDir)) {
+        for (Path entry : stream) {
+          if (Files.isDirectory(entry)) {
+            stack.push(entry);
+          } else if (Files.isRegularFile(entry)) {
+            if (entry.toString().length() > MAX_PATH_LENGTH) {
+              System.out.println(entry.toString());
+            }
+          }
+        }
+      } catch (AccessDeniedException e) {
+        System.err.println(e.getMessage());
+      } catch (IOException e) {
+        System.err.println("Ошибка доступа к директории: " + currentDir);
+        throw new RuntimeException(e);
+      }
+    }
+
+
+
+
+
+
+  }
+}
